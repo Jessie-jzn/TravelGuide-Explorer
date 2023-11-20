8f0785784b184358e3b2bb0f2ecf8c639ee703db
jessie <740395858@qq.com>
2023-11-21 00:18:54 +0800

    deploy

---
+++ app/components/NavBar/index.tsx
@@ -0,0 +1,7 @@
+import DehazeIcon from '@mui/icons-material/Dehaze';
+const NavBar = () =>{
+    return <div className='narbar-wrap'>
+        <DehazeIcon/> {/* 使用 SVG 图标 */}
+    </div>
+}
+export default NavBar
