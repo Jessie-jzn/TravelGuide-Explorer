22981c39327c8f8077967e6b6b322b91730a8e34
Jessie <740395858@qq.com>
2024-06-20 17:06:59 +0800

    fix: 样式问题

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-// module.exports = {
-//   plugins: ["postcss-import", "tailwindcss", "autoprefixer"]
-// }
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./components/**/*.js', './pages/**/*.js'],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
